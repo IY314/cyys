a214f847ab72daf812f2a6e719b72de21f1ff55f
Isaac Yee <48425750+IY314@users.noreply.github.com>
2022-06-26 18:32:58 -0700

    Add position struct

---
+++ src/pos.cpp
@@ -0,0 +1,15 @@
+#include "pos.hpp"
+
+void Pos::operator+=(const std::string &passed) noexcept
+{
+    for (const char &c : passed)
+    {
+        idx++, col++;
+
+        if (c == '\n')
+        {
+            ln++;
+            col = 0;
+        }
+    }
+};
---
+++ src/pos.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+struct Pos
+{
+    unsigned long long idx, ln, col;
+    std::string fname, ftext;
+
+    Pos(const Pos &o) noexcept = default;
+
+    Pos &operator=(const Pos &o) noexcept = default;
+
+    void operator+=(const std::string &passed) noexcept;
+};
