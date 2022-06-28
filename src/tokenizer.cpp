f43f9f43b0ea054663139277c8f6df70297e2330
Isaac Yee <48425750+IY314@users.noreply.github.com>
2022-06-28 10:26:49 -0700

    Add keyword tokenization

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -1,5 +1,6 @@
 #include "tokenizer.hpp"
 
+#include <algorithm>
 #include <iostream>
 #include <regex>
 
@@ -7,21 +8,22 @@
 #define RE_COMMENT R"(^#.*?\n|\r$)"
 #define RE_SPACE   R"(^\s+)"
 
+// Number rules
 #define RE_INT   R"(^\d+)"
 #define RE_FLOAT R"(^\d+\.\d+)"
 
 // ONLY SINGLE QUOTES
-#define RE_STRING R"(^'.*?')"
+#define RE_STRING "^'.*?'"
 
 // This means any of ( ) [ ] { } , . |, :, =, <, >, ==, !=, <=, >=
 #define RE_LIT R"(^(==)|(!=)|(<=)|(>=)|([()\[\]{},.|:=<>]))"
 
-#define RE_KEYWORD  // TODO
-
 // The classic identifier rule (as in C)
 #define RE_IDENT R"(^[[:alpha:]]\w*)"
 
-bool match(const std::string &str, const std::string &regex, std::string &res)
+bool match(const std::string &str,
+           const std::string &regex,
+           std::string &res) noexcept
 {
     // std::cout << "Matching " << str << " against " << regex << '\n';
     std::smatch m;
@@ -30,24 +32,55 @@ bool match(const std::string &str, const std::string &regex, std::string &res)
     return r;
 }
 
-#define matches(rgx) (match(ftext.substr(start.idx), rgx, res))
+class match_functor
+{
+    const std::string &str;
+    std::string &res;
 
-#define match_add_token(rgx, typ, cnv)                  \
-    if (matches(rgx))                                   \
-    {                                                   \
-        /* std::cout << "found " << res << std::endl;*/ \
-        pos += res;                                     \
-        tokens.push_back({typ, cnv, start, pos});       \
+public:
+    match_functor(const std::string &str, std::string &res) noexcept
+        : str(str),
+          res(res)
+    {
     }
+
+    bool operator()(const std::string &rgx) noexcept
+    {
+        return match(str, rgx, res);
+    }
+};
 
 std::vector<Token> tokenize(const std::string &fname,
                             const std::string &ftext) noexcept
 {
+#define matches(rgx) (match(ftext.substr(start.idx), rgx, res))
+
+#define match_add_token(rgx, typ, cnv)                       \
+    if (matches(rgx))                                        \
+    {                                                        \
+        pos += res;                                          \
+        std::cout << "Found " #typ << ": '" << res << "'\n"; \
+        tokens.push_back({typ, cnv, start, pos});            \
+    }
+    const auto keywords = std::vector<std::string>{"^require",
+                                                   "^struct",
+                                                   "^is",
+                                                   "^end",
+                                                   "^func",
+                                                   "^self",
+                                                   "^to",
+                                                   "^proc",
+                                                   "^let",
+                                                   "^if",
+                                                   "^do"};
+
     std::vector<Token> tokens;
 
     Pos pos{0, 1, 0, fname, ftext};
 
     std::string res;
+
+    std::vector<std::string>::const_iterator it;
 
     while (pos.idx < ftext.length())
     {
@@ -58,14 +91,22 @@ std::vector<Token> tokenize(const std::string &fname,
             match_add_token(RE_INT, Token::Type::Int, std::stoll(res))
         else match_add_token(RE_FLOAT, Token::Type::Float, std::stold(res))
         else match_add_token(RE_STRING, Token::Type::String, res)
-        // TODO: keyword
+        else if ((it = std::find_if(keywords.begin(),
+                                    keywords.end(),
+                                    match_functor(ftext.substr(pos.idx), res)))
+                 != keywords.end())
+        {
+            pos += res;
+            std::cout << "Found keyword: '" << res << "'\n";
+            tokens.push_back({Token::Type::Keyword, res, start, pos});
+        }
         else match_add_token(RE_IDENT, Token::Type::Ident, res)
         else if (matches(RE_LIT))
+#define lit_match(l, t) \
+    if (res == l) type = t, found = true
         {
             Token::LitType type;
             bool found = false;
-#define lit_match(l, t) \
-    if (res == l) type = t, found = true
             lit_match("(", Token::LitType::LParen);
             else lit_match(")", Token::LitType::RParen);
             else lit_match("[", Token::LitType::LBracket);
@@ -83,7 +124,12 @@ std::vector<Token> tokenize(const std::string &fname,
             else lit_match(">", Token::LitType::More);
             else lit_match("<=", Token::LitType::LessEquals);
             else lit_match(">=", Token::LitType::MoreEquals);
-            if (found) pos += res;
+            if (found)
+            {
+                pos += res;
+                std::cout << "Found literal: '" << res << "'\n";
+                tokens.push_back({Token::Type::Literal, type, start, pos});
+            }
 #undef lit_match
         }
 
@@ -91,4 +137,6 @@ std::vector<Token> tokenize(const std::string &fname,
     }
 
     return tokens;
+#undef matches
+#undef match_add_token
 }
